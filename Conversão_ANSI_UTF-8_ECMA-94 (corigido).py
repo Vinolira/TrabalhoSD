ca2f29d739dd755c412722cc8da562a1ba11a016
Vinolira <vinolira@gmail.com>
2025-04-20 16:36:55 -0400

    Add files via upload

---
+++ Conversão_ANSI_UTF-8_ECMA-94 (corigido).py
@@ -0,0 +1,58 @@
+def con_utf8(vetor):
+    texto_utf8 = ""
+    for i in range(0, len(vetor)):
+        if vetor[i] <= 127:
+            aux = hex(vetor[i])
+        elif vetor[i] > 127 and vetor[i] <= 191:
+            aux = hex(vetor[i])
+            texto_utf8 += "C2 "
+        elif vetor[i] > 191:
+            aux = hex(vetor[i]-64)
+            texto_utf8 += "C3 "
+        aux = str(aux)
+        aux = aux[2:]
+        aux = aux.upper()
+        texto_utf8 += aux
+        if i < len(vetor)-1:
+            texto_utf8 += " "
+    return texto_utf8
+
+def con_ecma94(vetor):
+    texto_ecma94 = ""
+    for i in range(0, len(vetor)):
+        if chr(vetor[i]) in """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ""":
+            aux = hex(vetor[i])
+            aux = str(aux)
+            aux = aux[2:]
+            aux = aux.upper()
+            texto_ecma94 += aux
+        else:
+            texto_ecma94 += "[NULL]"
+        if i < len(vetor)-1:
+            texto_ecma94 += " "
+    return texto_ecma94
+    
+
+
+# Aqui se espera uma entrada com o caminho relativo ou absoluto do arquivo.
+via = input("nome do arquivo: ")
+arc = open(via, 'rb') # isso talvez de errado
+scr = arc.read()
+
+# Converte o texto em um vetor de inteiros com os respectivos códigos de cada carácter
+vetor = []
+for i in range(0, len(scr)):
+    aux = scr[i]
+    vetor.append(aux)
+print(len(vetor))
+
+texto_utf8 = con_utf8(vetor)
+#print(texto_utf8)
+texto_ecma94 = con_ecma94(vetor)
+#print(texto_ecma94)
+fil = open("saída.txt", 'w')
+fil.write("UTF-8: " + texto_utf8 + "\n\nECMA-94: " + texto_ecma94)
+
+
+arc.close()
+fil.close()
